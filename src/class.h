8ab5bca10022dfc332a3ee9ef8531a5f43a5dafa
Mc-StArT <alesha@gilenko.net>
2025-12-10 11:57:11 +0300

    tweaks

--- src/class.h
+++ src/class.h
@@ -9,38 +9,50 @@ class MyClass {
 public:
 	/**
      * \~english @brief Enumerator example
-     * 
      * \~russian @brief Пример перечисления
 	 */
 	enum Scale
 	{
 		first = 5,  	/**< \~english Первый элемент 
                              \~russian First element */
-        second = 0,  	/**< \~russian Second element 
-                             \~english Второй элемент */
-		third = 3,		/**< Диапазон &plusmn;  8g */
-		fourth = 1, 	/**< Диапазон &plusmn;  16g */
-        elevenG = 4 	/**<  &plusmn;  16g */
+        second = 0,  	/**< \~english Second element 
+                             \~russian Второй элемент */
+		third = 3,		/**< \~english Third element 
+                             \~russian Третий элемент */
+		fourth = 1, 	/**< \~english Fourth element 
+                             \~russian Четвёртый элемент */
+        elevenG = 4 	/**< \~english Scale &plusmn;  16g
+                             \~russian Диапазон &plusmn;  16g */
 	};
 
     /**
-     * @brief Constructs a MyClass object with an initial value.
-     * @param initialValue The value to initialize the member variable with.
+     * \~english @brief Constructs a MyClass object with an initial value.
+     * \~english @param initialValue The value to initialize the member variable with.
+     * 
+     * \~russian @brief Создание объекта класса MyClass с заданным значением.
+     * \~russian @param initialValue Начальное значение внутреннего параметра.
      */
     MyClass(int initialValue);
 
     /**
-     * @brief Sets the value of the internal data.
-     * @param newValue The new value to set.
+     * \~english @brief Sets the value of the internal data.
+     * \~english @param newValue The new value to set.
+     * 
+     * \~russian @brief Устанoвить значение внутреннего параметра
+     * \~russian @param newValue Новое значение параметра
      */
     void setValue(int newValue);
 
     /**
-     * @brief Retrieves the current value of the internal data.
-     * @return The current value.
+     * \~english @brief Retrieves the current value of the internal data.
+     * \~english @return The current value.
+     *
+     * \~russian @brief Получить значение внутреннего параметра
+     * \~russian @return Значение параметра
      */
     int getValue() const;
 
 private:
-    int m_data; ///< @brief The internal data of the class.
+    int m_data; /*/< \~english @brief The internal data of the class.
+                     \~russian @brief Внутреннее значение класса */
 };
